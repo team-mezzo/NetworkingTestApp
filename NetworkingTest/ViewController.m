cf36d94f85a9a8f61ee172b3a5c7ca10e4dd20fa
16hchung <16hchung@castilleja.org>
2014-12-06 14:49:26 -0800

    "Load Donation" button loads json file from URL and NSLogs the returned dictionary

--- NetworkingTest/ViewController.m
+++ NetworkingTest/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
+#warning add base URL below
+static NSString * const baseURLString = @"INSERT BASE URL";
+
+
 @interface ViewController ()
+
+@property (nonatomic, strong) NSDictionary *donation;
 
 @end
 
@@ -22,6 +28,28 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)loadButtonTapped:(id)sender {
+    NSURL *baseURL = [NSURL URLWithString:baseURLString];
+    NSDictionary *parameters = @{@"format" : @"json"};
+    
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:baseURL];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+
+#warning add key below
+    [manager GET:@"INSERT KEY" parameters:parameters success:^(NSURLSessionDataTask *task, id responseObject) {
+        self.donation = responseObject;
+        NSLog(@"%@", self.donation);
+        
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Donation"
+                                                            message:[error localizedDescription]
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"Ok"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+    }];
 }
 
 @end
